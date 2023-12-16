17cc758febd9cfee967ee14d07a025660f433549
Joseangvas <developer.javp@gmail.com>
2023-12-16 11:18:20 -0400

    Creando Archivos php del Modelo

--- app/Models/viewsModel.php
+++ app/Models/viewsModel.php
@@ -8,4 +8,39 @@
 		protected function obtenerVistasModelo($vista){
 
 			$listaBlanca=["dashboard","cashierNew","cashierList","cashierSearch","cashierUpdate","userNew","userList","userUpdate","userSearch","userPhoto","clientNew","clientList","clientSearch","clientUpdate","categoryNew","categoryList","categorySearch","categoryUpdate","productNew","productList","productSearch","productUpdate","productPhoto","productCategory","companyNew","saleNew","saleList","saleSearch","saleDetail","logOut"];
+		/*----------  Funcion para ejecutar una consulta UPDATE preparada  ----------*/
+		protected function actualizarDatos($tabla,$datos,$condicion){
+
+			$query="UPDATE $tabla SET ";
+
+			$C=0;
+			foreach ($datos as $clave){
+				if($C>=1){ $query.=","; }
+				$query.=$clave["campo_nombre"]."=".$clave["campo_marcador"];
+				$C++;
+			}
+
+			$query.=" WHERE ".$condicion["condicion_campo"]."=".$condicion["condicion_marcador"];
+
+			$sql=$this->conectar()->prepare($query);
+
+			foreach ($datos as $clave){
+				$sql->bindParam($clave["campo_marcador"],$clave["campo_valor"]);
+			}
+
+			$sql->bindParam($condicion["condicion_marcador"],$condicion["condicion_valor"]);
+
+			$sql->execute();
+
+			return $sql;
+		}
+
+
+		/*---------- Funcion eliminar registro ----------*/
+        protected function eliminarRegistro($tabla,$campo,$id){
+            $sql=$this->conectar()->prepare("DELETE FROM $tabla WHERE $campo=:id");
+            $sql->bindParam(":id",$id);
+            $sql->execute();
             
+            return $sql;
+        }
