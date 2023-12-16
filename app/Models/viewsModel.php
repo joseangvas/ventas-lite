fee25d6e32ae00762cc7b1ebd82845f3379c6a53
Joseangvas <developer.javp@gmail.com>
2023-12-16 11:20:56 -0400

    Creando Modelos

--- app/Models/viewsModel.php
+++ app/Models/viewsModel.php
@@ -44,3 +44,18 @@
             
             return $sql;
         }
+        if(in_array($vista, $listaBlanca)){
+            if(is_file("./app/views/content/".$vista."-view.php")){
+                $contenido="./app/views/content/".$vista."-view.php";
+            }else{
+                $contenido="404";
+            }
+        }elseif($vista=="login" || $vista=="index"){
+            $contenido="login";
+        }else{
+            $contenido="404";
+        }
+        return $contenido;
+    }
+
+}
